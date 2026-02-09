213987b8901c6be3f3186affda150ae422f75f4d
FrugalITDad <wcouse3@gmail.com>
2026-02-08 23:22:25 -0500

    Wizard Code Change

--- repo/plugin.program.cutcablewizard/service.py
+++ repo/plugin.program.cutcablewizard/service.py
@@ -1,6 +1,5 @@
 import xbmc, xbmcaddon, xbmcvfs, os, json, xbmcgui
 
-# --- CONFIGURATION ---
 ADDON = xbmcaddon.Addon()
 ADDON_DATA = xbmcvfs.translatePath(ADDON.getAddonInfo('profile'))
 TRIGGER_FILE = os.path.join(ADDON_DATA, 'trigger.txt')
@@ -11,47 +10,31 @@ def run_json(method, params):
     return xbmc.executeJSONRPC(json.dumps(query))
 
 def apply_total_recovery():
-    xbmc.log("--- [Wizard Service] Starting Global Addon Recovery...", xbmc.LOGINFO)
-    
-    # 1. GET A LIST OF ALL INSTALLED ADDONS
-    # We ask Kodi for everything currently sitting in the addons folder
+    # 1. THE BULK ENABLE
+    # We fetch every addon and force it ON. This fixes the Repo and Wizard being disabled.
     result = run_json("Addons.GetAddons", {"installed": True})
     data = json.loads(result)
-    
     if 'result' in data and 'addons' in data['result']:
-        all_addons = data['result']['addons']
-        
-        # 2. THE GLOBAL ENABLE LOOP
-        # This turns ON every addon (Skin, PVR, Scripts, etc.)
-        for item in all_addons:
-            a_id = item['addonid']
-            run_json("Addons.SetAddonEnabled", {"addonid": a_id, "enabled": True})
+        for item in data['result']['addons']:
+            run_json("Addons.SetAddonEnabled", {"addonid": item['addonid'], "enabled": True})
     
     xbmc.sleep(2000)
 
-    # 3. FORCE THE SKIN GUI SWITCH
-    # Now that the skin addon is definitely enabled, we apply the look
+    # 2. THE SKIN SWITCH
     run_json("Settings.SetSettingValue", {"setting": "lookandfeel.skin", "value": SKIN_ID})
     
-    xbmc.sleep(1000)
-
-    # 4. TRIGGER IPTV MERGE & PVR RELOAD
+    # 3. PVR & IPTV MERGE
     if os.path.exists(xbmcvfs.translatePath('special://home/addons/plugin.iptvmerge')):
         xbmc.executebuiltin('RunAddon(plugin.iptvmerge, "merge")')
-    
     xbmc.executebuiltin('UpdatePVRByAddon(pvr.iptvsimple)')
     
-    # 5. CLEANUP
     if os.path.exists(TRIGGER_FILE):
         os.remove(TRIGGER_FILE)
-    
-    xbmcgui.Dialog().notification("Wizard", "All Addons & Skin Restored!", xbmcgui.NOTIFICATION_INFO, 5000)
+    xbmcgui.Dialog().notification("Wizard", "Build fully activated!", xbmcgui.NOTIFICATION_INFO, 5000)
 
 if __name__ == '__main__':
     monitor = xbmc.Monitor()
-    # 20 seconds is the "Golden Window" for Fire Sticks to finish their background scan
-    if monitor.waitForAbort(20): 
-        exit()
-
+    # 30 second wait is now required because we are enabling the WHOLE system.
+    if monitor.waitForAbort(30): exit()
     if os.path.exists(TRIGGER_FILE):
         apply_total_recovery()
